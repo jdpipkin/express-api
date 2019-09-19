7d2d95112d9db51a146d279ff4e55fd6bb392f29
Brandon Johnson <C-Brandon.Johnson1@charter.com>
2019-09-19 16:11:37 -0500

    [BHJ] Updating port number and removing unneeded deps for helloWorld

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 const express = require('express')
-var morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
 const config = require('./config')
 
 const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser)
-app.use(cors)
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/brandon', (req, res) => res.send('hello world'))
 
 app.listen(config.port, () => console.log('Listening on port: 8080'))
